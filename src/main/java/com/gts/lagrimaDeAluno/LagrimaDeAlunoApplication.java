1b6f9eeb6e8fdd71c0e5b30ca628a83ebadb2690
Gabriel Trabuco <trabuco2206@gmail.com>
2025-10-02 22:14:17 -0300

    add

---
+++ src/main/java/com/gts/lagrimaDeAluno/LagrimaDeAlunoApplication.java
@@ -0,0 +1,13 @@
+package com.gts.lagrimaDeAluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagrimaDeAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LagrimaDeAlunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gts/lagrimaDeAluno/LagrimaDeAlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gts.lagrimaDeAluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagrimaDeAlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
