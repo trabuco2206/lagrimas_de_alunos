19a8635767d9aff09807351bffa9ca7dac166662
Gabriel Trabuco <trabuco2206@gmail.com>
2025-10-13 20:16:12 -0300

    Adicionei

--- src/main/java/com/gts/lagrimaDeAluno/users/UserRepository.java
+++ src/main/java/com/gts/lagrimaDeAluno/users/UserRepository.java
@@ -1,4 +1,8 @@
 package com.gts.lagrimaDeAluno.users;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+    UserDetails findByLogin(String Login);
 }
